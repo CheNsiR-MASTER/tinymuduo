78b1079323ccdecb15ac5f2c21c2991effe44474
CheNsiR-MASTER <378909196@qq.com>
2022-05-04 23:18:49 +0800

    first commit

---
+++ build/CMakeFiles/tinymuduo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tinymuduo.dir/Logger.o"
+  "CMakeFiles/tinymuduo.dir/Timestamp.o"
+  "../lib/libtinymuduo.pdb"
+  "../lib/libtinymuduo.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tinymuduo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
